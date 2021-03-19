886af0e36ed7eef24d8b5160d860ab0dce2fd071
Chris Churas <churas.camera@gmail.com>
2021-03-18 17:28:48 -0700

    bumped to 0.2.0

--- cdapsutil/__init__.py
+++ cdapsutil/__init__.py
@@ -2,7 +2,9 @@
 
 __author__ = 'Chris Churas'
 __email__ = 'churas.camera@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
-from .cd import CommunityDetectionError
 from .cd import CommunityDetection
+from .exceptions import CommunityDetectionError
+from .runner import DockerRunner
+from .runner import ServiceRunner
