ba3a12fee2a12a7e93d6a5e628b5fb0517da87cd
Chris Churas <churas.camera@gmail.com>
2024-02-09 15:22:32 -0800

    removed a1

--- cdapsutil/__init__.py
+++ cdapsutil/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Chris Churas'
 __email__ = 'churas.camera@gmail.com'
-__version__ = '0.2.2a1'
+__version__ = '0.2.2'
 
 from .cd import CommunityDetection
 from .exceptions import CommunityDetectionError
