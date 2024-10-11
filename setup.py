4596646a19adf960b97f3535bbab92238d816247
dzoanka <joanna.lenkiewicz@gmail.com>
2024-10-11 12:14:07 -0700

    Switch to pytest

--- setup.py
+++ setup.py
@@ -22,10 +22,6 @@ requirements = [
     'tqdm'
 ]
 
-test_requirements = [
-    'requests-mock'
-]
-
 setup(
     name='cdapsutil',
     version=version,
@@ -56,7 +52,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
+    ]
 )
